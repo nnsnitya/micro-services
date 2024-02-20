afa3d4d1ec40656273fb0833cc2521db2ebf8359
Nityanand Singh <nns.nitya@gmail.com>
2024-02-21 00:15:08 +0530

    Feign-Client: to call Rating Service

--- user_service/src/test/java/com/user/service/UserServiceApplicationTests.java
+++ user_service/src/test/java/com/user/service/UserServiceApplicationTests.java
@@ -1,7 +1,12 @@
 package com.user.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import com.user.service.entities.Rating;
+import com.user.service.external.services.RatingService;
 
 @SpringBootTest
 class UserServiceApplicationTests {
@@ -10,4 +15,16 @@ class UserServiceApplicationTests {
 	void contextLoads() {
 	}
 
+	@Autowired
+	private RatingService ratingService;
+	
+	@Test
+	void createRating() {
+		Rating rating = Rating.builder().rating(10).ratingId("").hotelId("").feedback("this is created using feign client").build();
+		ResponseEntity<Rating> ratingRespEntt = ratingService.createRating(rating);
+		System.out.println(ratingRespEntt.getBody().getFeedback());
+		System.out.println(ratingRespEntt.getStatusCode());
+		
+		System.out.println("new rating created");
+	}
 }
